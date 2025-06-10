b3fcba4e5022afcfc0ede2d0572a3b5bc7261734
Eli4ka0309 <100150012+Eli4ka0309@users.noreply.github.com>
2025-06-11 02:55:02 +0300

    Change Burger
    Change Burger

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-fun
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".header_nav-burger");
+  const nav = document.querySelector(".header_nav-ul");
+
+  if (burger && nav) {
+    burger.addEventListener("click", () => {
+ 
+      nav.classList.toggle("open");
+
+      burger.classList.toggle("active_burger");
+    });
+  }
+});
